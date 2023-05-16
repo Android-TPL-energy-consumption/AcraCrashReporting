953b5c7d4aeac997f77dc1651f63399be72fd36a
Remy Raes <contact@remyraes.com>
2023-05-17 00:08:06 +0200

    feat: add ACRA configuration

--- app/src/main/java/tpl/crashreporting/acra/MainActivity.java
+++ app/src/main/java/tpl/crashreporting/acra/MainActivity.java
@@ -4,12 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.acra.ACRA;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.HttpSenderConfiguration;
+import org.acra.config.HttpSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ACRA.init(getApplication(), new CoreConfigurationBuilder()
+                //core configuration:
+                .withBuildConfigClass(BuildConfig.class)
+                .withReportFormat(StringFormat.JSON)
+        );
+
+        HttpSenderConfiguration endpoint = new HttpSenderConfigurationBuilder()
+                .withUri("https://your.server.com/report")
+                .build();
+        System.out.println(endpoint.enabled());
     }
 
     public void onButtonClick(android.view.View view) {
